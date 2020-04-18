8332f2e4a4a1d618fbb92b6112f48cd827132f1e
Mario <a01187827@itesm.mx>
2020-04-18 11:40:12 -0600

    init

---
+++ javascriptFunctions.js
@@ -0,0 +1,10 @@
+//This function returns the addition of two values
+function sum(a, b){
+    return a + b;
+}
+// This function returns a decimal value converted to an octal string
+function decimalToOctal(decimal){
+    return decimal.toString(8);
+}
+exports.sum = sum;
+exports.decimalToOctal = decimalToOctal;
